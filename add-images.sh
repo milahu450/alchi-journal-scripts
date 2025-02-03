0b6d232381b36744f3fda568c0a78b633aebee79
Milan Hauth <milahu@gmail.com>
2025-02-03 15:03:04 +0100

    up

--- add-images.sh
+++ add-images.sh
@@ -4,9 +4,20 @@ set -eux
 
 shopt -s nullglob
 
-./move-images.sh
+cd "$(dirname "$0")"/..
 
-./generate-index-files.sh
+# if [ "$0" != "./scripts/add-images.sh" ]; then
+if ! [ -e "scripts/add-images.sh" ]; then
+if false; then
+  echo "error: you must run this as"
+  echo "  ./scripts/add-images.sh"
+  echo "  from the alchi-journal repo"
+  exit 1
+fi
+
+./scripts/move-images.sh
+
+./scripts/generate-index-files.sh
 
 git add img/*/*.webp
 git add html/????-??.html
--- move-images.sh
+++ move-images.sh
@@ -5,22 +5,37 @@
 
 
 
-find . -mindepth 1 -maxdepth 1 -type f -regextype posix-extended -regex '\./[0-9]{4}-[0-9]{2}-[0-9]{2}\.[0-9]{2}-[0-9]{2}.*\.webp' -printf '%P\n' |
+find . -mindepth 1 -maxdepth 1 -type f \
+  -regextype posix-extended -regex \
+  '\./[0-9]{4}-?[0-9]{2}-?[0-9]{2}[\._][0-9]{2}-?[0-9]{2}.*\.webp' -printf '%P\n' |
 while read webp_path
 do
 
-  year_month=$(echo "$webp_path" | sed -E 's/^([0-9]{4}-[0-9]{2})-.*$/\1/')
+  year_month=$(echo "$webp_path" |
+    sed -E 's/^([0-9]{4}-?[0-9]{2})-?.*$/\1/')
 
   if [[ "$year_month" == "$webp_path" ]]; then
     echo "error: failed to parse year_month from webp_path: $webp_path"
     exit 1
   fi
 
+  if [ ${#year_month} == 6 ]; then
+    year_month=${year_month:0:4}-${year_month:4:2}
+  fi
+
   #echo "$year_month $webp_path"
 
   mkdir -p "img/$year_month"
 
-  output_path="img/$year_month/$(basename "$webp_path")"
+  base="$(basename "$webp_path")"
+
+  if echo "$base" | grep -q -E '^[0-9]{4}[0-9]{2}[0-9]{2}_[0-9]{2}[0-9]{2}.*\.webp'; then
+    base=$(echo "$base" |
+      sed -E 's/^([0-9]{4})([0-9]{2})([0-9]{2})_([0-9]{2})([0-9]{2})/\1-\2-\3.\4-\5/'
+    )
+  fi
+
+  output_path="img/$year_month/$base"
 
   if [[ -e "$output_path" ]]; then
     echo "error: output file exists: $output_path"
@@ -28,5 +43,8 @@ do
   fi
 
   mv "$webp_path" "$output_path"
+
+  # fix: files on sdcard have mode 0777
+  chmod 0644 "$output_path"
 
 done
