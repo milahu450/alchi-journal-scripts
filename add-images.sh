4a05338ca943e02567f1acb9d7a7cb4b967b7243
Milan Hauth <milahu@gmail.com>
2025-02-05 15:30:22 +0100

    up add-images.sh

--- add-images.sh
+++ add-images.sh
@@ -8,16 +8,22 @@ cd "$(dirname "$0")"/..
 
 # if [ "$0" != "./scripts/add-images.sh" ]; then
 if ! [ -e "scripts/add-images.sh" ]; then
-if false; then
+# if false; then
   echo "error: you must run this as"
   echo "  ./scripts/add-images.sh"
   echo "  from the alchi-journal repo"
   exit 1
 fi
 
-./scripts/move-images.sh
+# needed for scripts/generate-index-files.sh
+if ! command -v pandoc; then
+  echo "error: pandoc is not installed"
+  exit 1
+fi
 
-./scripts/generate-index-files.sh
+bash ./scripts/move-images.sh
+
+bash ./scripts/generate-index-files.sh
 
 git add img/*/*.webp
 git add html/????-??.html
